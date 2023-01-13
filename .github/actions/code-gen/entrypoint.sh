afccfcaf40955721603a3a9294ff0c2e26ff9284
wagowa5 <wagowagonsha@gmail.com>
2023-01-13 13:04:41 +0900

    test

--- .github/actions/code-gen/entrypoint.sh
+++ .github/actions/code-gen/entrypoint.sh
@@ -40,7 +40,7 @@ java -jar ${JAR_PATH} generate \
 ## generate kotlin-spring
 java -jar ${JAR_PATH} generate \
   -i ./dist/openapi/openapi.yaml \
-  -g org.openapitools.codegen.languages.TlbKotlinSpringServerCodegen \
+  -g org.openapitools.codegen.languages.KotlinSpringServerCodegen \
   #-t ./config/spring/mustache
   #-c ./config/spring/config.yaml \
   -o kotlin-spring \
